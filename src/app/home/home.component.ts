3f2398edbe17df349c9360c829cdd03e34ca9e25
Muthu Krishnan <muthumk2812@gmail.com>
2024-05-28 17:03:01 +0530

    commit

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,71 +1,86 @@
 import { Component, Inject, NgModule, NgModuleRef, OnDestroy, OnInit, inject } from '@angular/core';
 import { Firestore } from '@angular/fire/firestore';
 import { DataService } from '../service/data.service';
-import { CommonModule} from '@angular/common';
-import {FormsModule} from '@angular/forms'
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms'
 import { DestroyRef } from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule,FormsModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent implements OnInit, OnDestroy{
-  name:any
-  age:any
-  phoneNumber:any
-  posts:any
-  id:any
-  updateName:string=''
-  updateAge:number=0
-  updatePhoneNumber:string=''
-  takeDestroy= inject(DestroyRef)
-  constructor(private service:DataService){}
-  
+export class HomeComponent implements OnInit, OnDestroy {
+  name: any
+  age: any
+  phoneNumber: any
+  posts: any
+  id: any
+  updateName: string = ''
+  updateAge: number = 0
+  updatePhoneNumber: string = ''
+  takeDestroy = inject(DestroyRef)
+  constructor(private service: DataService) { }
+
   ngOnInit(): void {
-    this.service.getData().pipe(takeUntilDestroyed(this.takeDestroy)).subscribe((res:any)=>{
-      this.posts=res
+    this.service.getData().pipe(takeUntilDestroyed(this.takeDestroy)).subscribe((res: any) => {
+      this.posts = res
       console.log(this.posts);
     })
   }
   ngOnDestroy(): void {
     this.takeDestroy.onDestroy
   }
-  async onSave(){
-    if(this.name.trim()&&this.age !==null && this.phoneNumber.trim()){
-      const user ={
-        name:this.name,
-        age:this.age,
-        phoneNumber:this.phoneNumber
+  async onSave() {
+    if (this.name.trim() && this.age !== null && this.phoneNumber.trim()) {
+      const user = {
+        name: this.name,
+        age: this.age,
+        phoneNumber: this.phoneNumber
       }
-      try{
+      try {
         await this.service.addUser(user)
         console.log('user added successfully');
-        this.name='',
-        this.age=null,
-        this.phoneNumber=''
+        this.name = '',
+          this.age = null,
+          this.phoneNumber = ''
       }
-      catch(error){
-        console.error('Error adding User',error);
+      catch (error) {
+        console.error('Error adding User', error);
       }
     }
   }
-   onsetDoc(postId:any){
+  async defaultSave() {
+    try {
+      const user = {
+        name: 'MuthuKrishnan',
+        age: 25,
+        phoneNumber: '8946021225'
+      }
+      await this.service.addUser(user)
+      alert('user added successfully');
+    }
+    catch (error) {
+      console.error('defaultsave as some error', error);
+    }
+  }
+
+  onsetDoc(postId: any) {
     this.service.setUser(postId)
   }
-  deletePost(postId:any){
+  deletePost(postId: any) {
     this.service.DeleteUser(postId)
   }
-  updatePost(postId:any){
-    this.service.name=this.updateName
-    this.service.age=this.updateAge
-    this.service.phoneNumber=this.updatePhoneNumber
+  updatePost(postId: any) {
+    this.service.name = this.updateName
+    this.service.age = this.updateAge
+    this.service.phoneNumber = this.updatePhoneNumber
     this.service.UpdataUser(postId)
-    this.updateName=''
-    this.updateAge=0
-    this.updatePhoneNumber=''
+    this.updateName = ''
+    this.updateAge = 0
+    this.updatePhoneNumber = ''
   }
 }
